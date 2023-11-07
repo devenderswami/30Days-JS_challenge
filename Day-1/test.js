9daf0a8d12e1d469e67188d88a47412167321378
devenderswami53@gmail.com <devenderswami@Devenders-MacBook-Pro.local>
2023-11-07 23:12:53 +0530

    second day

--- Day-1/test.js
+++ Day-1/test.js
@@ -1,10 +1,25 @@
+// function reverseNumber(num){
+
+//     let ans = num.toString().split('').reverse().join('')
+//     console.log("+++ ans",parseInt(ans))
+
+
+// }
+
 function reverseNumber(num){
 
-    let ans = num.toString().split('').reverse().join('')
-    console.log("+++ ans",parseInt(ans))
+    let reverseNum = 0 
+    while(num != 0){
+        let rem = num%10
+        reverseNum = reverseNum*10 + rem
+        num = Math.floor(num/10)
+        
+    }
+   return  reverseNum
 
 
 }
 
 
-reverseNumber(-32243)
+let result  = reverseNumber(32243)
+console.log(result)
---
+++ Day-2/test.js
@@ -0,0 +1,23 @@
+// Write a JavaScript function that accepts a string as a parameter and converts the first letter of each word into upper case.
+
+// Example string: 'the quick brown fox'
+// Expected Output: 'The Quick Brown Fox '
+
+function convertUpperCase(arr){
+    let A = arr.split(" ")
+    let str = ''
+    //let result = str.charAt(0).toUpperCase() 
+
+     for(let i = 0 ; i< A.length; i++){
+        
+        A[i] = A[i].charAt(0).toUpperCase() + A[i].slice(1)
+     }
+
+     const str2 = A.join(" ");
+    console.log(str2)
+
+
+}
+
+
+convertUpperCase('the quick brown fox')
